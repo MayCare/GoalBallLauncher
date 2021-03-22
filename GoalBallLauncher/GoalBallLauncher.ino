df31110aa9f01c5afabca6cfdfb5a684eb1c996a
MayCare <makerzmailbox@gmail.com>
2021-03-22 13:40:45 +0200

    add control over ball load Actuator upon interrupt on pin 2

--- GoalBallLauncher/GoalBallLauncher.ino
+++ GoalBallLauncher/GoalBallLauncher.ino
@@ -1,24 +1,64 @@
+#define BRAKE 0
+#define CW    1
+#define CCW   2
+
 #define PWM_MAX_VAL             255
 #define SPEED_V_MIN_VAL         1
 #define SPEED_V_MAX_VAL         5
-#define PIN_PWM_R_MOTOR         5
-#define PIN_PWM_L_MOTOR         6
 
-#define MAX_SERIAL_STR_LEN      2
+//MOTOR R
+#define PIN_PWM_R_MOTOR                    5
+#define DEFAULT_SPEED_VALUE_R_MOTOR_volt   4
 
-char userInput[MAX_SERIAL_STR_LEN] = "";
+//MOTOR L
+#define PIN_PWM_L_MOTOR                    6
+#define DEFAULT_SPEED_VALUE_R_MOTOR_volt   4
+
+//LAUNCH BALL ACTUATOR
+#define LOAD_BALL_ACTUATOR_CW_PIN     7
+#define LOAD_BALL_ACTUATOR_CCW_PIN    8
+#define LOAD_BALL_ACTUATOR_SPEED_PIN  4
+#define LOAD_BALL_ACTUATOR_EN_PIN     4
+
+
+//Note: in order to shoot, hold in pin button 4 pressed,
+//Then, while 4 is pressed, press button in pin 2
+#define PIN_SHOOT_ENABLE      13
+#define PIN_SHOOT_INTERRUPT   2
+
+//TODO - replace this boolean flag with multiple states (more like a state machine).
+bool g_shootPressed = false;
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Here We Go!");
-  Serial.println("Choose Motor by R / L");
-  Serial.println("Choose speed level between  0 -> 5");
-  Serial.println("input i.e. - R1");
-  Serial.println("input i.e. - L4");
 
-  setPwmFrequency(PIN_PWM_R_MOTOR, 1);
+  // NOTE! setting the frequency changes the clock and affects delay().
+  //USE ONLY IF MUST! Then TEST and ADJUST delay() usages in the code accordingly
+  //setPwmFrequency(PIN_PWM_R_MOTOR, 1);
+  //setPwmFrequency(PIN_PWM_L_MOTOR, 1);
+
+  setupUserInterface();
+  setupLoadBallActuator();
+  
+  attachInterrupt(digitalPinToInterrupt(PIN_SHOOT_INTERRUPT), shootButtonISR, FALLING); // trigger when button pressed, but not when released.
 }
 
+
+void loop() {
+  if (g_shootPressed && isShootingEnabled()) {
+    Serial.println("If you have to shoot - SHOOT!"); // don't talk :)
+    shoot();
+  }
+}
+
+bool isShootingEnabled() {
+  //<DEBUG CODE!!>
+  return true;
+  //</DEBUG CODE!!>  
+  //COMMENT IN the line below when done debugging. 
+  //This is a saftey mechanism:
+  //return (digitalRead(PIN_SHOOT_ENABLE) == LOW);
+}
 
 void setPwmFrequency(int pin, int divisor) {
   byte mode;
@@ -51,64 +91,61 @@ void setPwmFrequency(int pin, int divisor) {
   }
 }
 
-void loop() {
-  if (! getInput()) {
-    return;
-  }
-  char motorChoice = userInput[0];
-  Serial.print("motorChoice is "); Serial.println(motorChoice);
-  
-  if (! getInput()) {
-    return;
-  }
-  int valuePWM = atoi((char*)&userInput[1]);
-  Serial.print("valuePWM is "); Serial.println(valuePWM);
-  
-  int pinPWM = 3;
-  switch (motorChoice) {
-    case 'R':
-      pinPWM = PIN_PWM_R_MOTOR;
-      break;
-    case 'L':
-      pinPWM = PIN_PWM_L_MOTOR;
-      break;
-    default:
-      Serial.println("ERR- illigal motor choice");
-      break;
-  }
-  
-  setPWM(pinPWM, valuePWM);
+void setupUserInterface() {
+  pinMode(PIN_SHOOT_ENABLE, INPUT_PULLUP);
+  pinMode(PIN_SHOOT_INTERRUPT, INPUT_PULLUP);
 }
 
-bool getInput() {
-  int index = 0;
-  bool hasInput = false;
-
-  if (index == MAX_SERIAL_STR_LEN) {
-    Serial.println("ERR: input too long");
-  }
-
-  while (Serial.available() && index < MAX_SERIAL_STR_LEN) {
-    hasInput = true;
-    char ch = Serial.read();
-    if (ch != 10) {
-      userInput[index++] = ch;
-    }
-    else {
-      userInput[index] = '\0';
-      Serial.print("input is ");
-      Serial.println(userInput);
-    }
-
-    delay(100);
-  }
-
-  return hasInput;
+void setupLoadBallActuator() {
+  pinMode(LOAD_BALL_ACTUATOR_CW_PIN, OUTPUT);
+  pinMode(LOAD_BALL_ACTUATOR_CCW_PIN, OUTPUT);
+  pinMode(LOAD_BALL_ACTUATOR_SPEED_PIN, OUTPUT);
+  pinMode(LOAD_BALL_ACTUATOR_EN_PIN, OUTPUT);
 }
 
 void setPWM(int ledPin, int speedV) {
   int pwmVal = map(speedV, SPEED_V_MIN_VAL, SPEED_V_MAX_VAL, 0, PWM_MAX_VAL);
   Serial.println(pwmVal);
-  analogWrite(ledPin, pwmVal);
   delay(2000);
+}
+
+
+void shootButtonISR() {
+  g_shootPressed = true;
+  //TODO Add timeout mechanism to elapse flags.
+}
+
+
+void moveLoadBallActuator(int dir) {
+  digitalWrite(LOAD_BALL_ACTUATOR_EN_PIN, HIGH);
+  if (dir == CW)
+    {
+      digitalWrite(LOAD_BALL_ACTUATOR_CW_PIN, LOW);
+      digitalWrite(LOAD_BALL_ACTUATOR_CCW_PIN, HIGH);
+    }
+    else if (dir == CCW)
+    {
+      digitalWrite(LOAD_BALL_ACTUATOR_CW_PIN, HIGH);
+      digitalWrite(LOAD_BALL_ACTUATOR_CCW_PIN, LOW);
+    }
+    else
+    {
+      digitalWrite(LOAD_BALL_ACTUATOR_CW_PIN, LOW);
+      digitalWrite(LOAD_BALL_ACTUATOR_CCW_PIN, LOW);
+      digitalWrite(LOAD_BALL_ACTUATOR_SPEED_PIN, LOW);
+    }
+}
+
+
+void shoot() {
+  g_shootPressed = false;
+  
+  moveLoadBallActuator(CW);
+  delay(2000);
+  moveLoadBallActuator(CCW);
+  delay(2000);
+  moveLoadBallActuator(BRAKE);
+
+  //move motors by:
+  //setPWM(PIN_PWM_R_MOTOR, valuePWM); // to set speed of motors
 }
